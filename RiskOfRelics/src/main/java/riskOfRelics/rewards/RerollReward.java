ba50a0abd307c280b6cc2651a9e387025769ee90
tetraminus <54343976+tetraminus@users.noreply.github.com>
2023-04-11 18:25:55 -0700

    pennies texture

--- RiskOfRelics/src/main/java/riskOfRelics/rewards/RerollReward.java
+++ RiskOfRelics/src/main/java/riskOfRelics/rewards/RerollReward.java
@@ -1,21 +1,12 @@
 package riskOfRelics.rewards;
 
 import basemod.abstracts.CustomReward;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 import riskOfRelics.patches.RerollRewardPatch;
 import riskOfRelics.relics.Recycler;
 import riskOfRelics.util.TextureLoader;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.combatRewardScreen;
-import static com.megacrit.cardcrawl.dungeons.AbstractDungeon.player;
-import static riskOfRelics.DefaultMod.makeRelicPath;
 
 public class RerollReward extends CustomReward {
     public static String iconpath = "riskOfRelicsResources/images/ui/reward/RerollReward.png";
@@ -23,21 +14,9 @@ public class RerollReward extends CustomReward {
 
     public RerollReward() {
         super(ICON, Recycler.RerollText, RerollRewardPatch.RISKOFRELICS_REROLL);
-//        for (RewardItem r: AbstractDungeon.getCurrRoom().rewards) {
-//            relicLink = r;
-//
-//        }
-
-
     }
-
-
-
     @Override
     public boolean claimReward() {
-
-
-
         for (RewardItem reward : AbstractDungeon.getCurrRoom().rewards) {
             if (reward.type == RewardItem.RewardType.RELIC && !reward.isDone && !reward.ignoreReward) {
                 reward.relic = AbstractDungeon.returnRandomRelic(AbstractDungeon.returnRandomRelicTier());
